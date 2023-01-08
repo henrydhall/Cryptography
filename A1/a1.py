9f9d0fe2cf1bc9cc996bb04eb8fdae9eb4096c78
HenrydHall <henrydhall3.14@gmail.com>
2023-01-07 21:33:45 -0700

    Problem 1.12 done

--- A1/a1.py
+++ A1/a1.py
@@ -25,6 +25,38 @@ def extended_euclidean(a,b):
     y = x1
  
     return gcd, x, y
+
+# Problem 1.12 Improved Extended Euclidean Algorithm
+def improved_euclidean(a,b):
+    # Catching b = 0
+    if b == 0:
+        return a, 1, 0
+    u = 1
+    g = a
+    x = 0
+    y = b
+    while y != 0:
+        q, t = g//y, g%y
+        s = u-(q*x)
+        u = x
+        g = y
+        x = s
+        y = t
+            
+    v = (g-(a*u))/b
+    if u <= 0:
+        u = u + (b/g)
+        v = v - (a/g)
+    return g, u, v
+    
+def prob_1_12():
+    print('Prob 1.12')
+    print( improved_euclidean(527, 1258) )
+    print( improved_euclidean(228, 1056) )
+    print( improved_euclidean(163961, 167181) )
+    print( improved_euclidean(3892394, 239847) )
+
+    
  
 if __name__ == '__main__':
     print( euclidean(139024789,93278890) )
@@ -32,3 +64,5 @@ if __name__ == '__main__':
     print( extended_euclidean(291, 252) )
     print( extended_euclidean(16261, 85652) )
     print( extended_euclidean(4,7))
+    print( improved_euclidean(4,7))
+    prob_1_12()
