6305975e04181f9af014300ff1ca619bc28c89d3
HenrydHall <henrydhall3.14@gmail.com>
2023-01-08 11:04:02 -0700

    Fast Power algorithm

--- A2/a2.py
+++ A2/a2.py
@@ -5,4 +5,14 @@ Assignment 2 - Henry Hall
 
 # Problem 1.24 - The Fast Power Algorithm
 def fast_power(N, g, A):
-    raise NotImplementedError('TODO: fast_power algo')
+    a = g
+    b = 1
+    while A > 0:
+        if A%2 == 1:
+            b = (b*a) % N
+        a = (a**2) % N
+        A = A//2
+    return b
+
+if __name__ == '__main__':
+    print( fast_power(21, 9, 30) )
