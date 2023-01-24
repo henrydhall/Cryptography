b12328db9542a7c01a9f8c19cab37ee533aaa258
HenrydHall <henrydhall3.14@gmail.com>
2023-01-23 21:59:11 -0700

    Kind of a mess, stuck on primitive roots

--- A3/a3.py
+++ A3/a3.py
@@ -1,3 +1,8 @@
+"""
+a3.py
+Assignment 3 - Henry Hall
+"""
+
 def extended_euclidean(a,b):
     if a == 0:
         return b, 0, 1
@@ -29,9 +34,44 @@ def ee_multiplicative_inverse(p,a):
 def fp_multiplicative_inverse(p,a):
     return fast_power(p, a, p-2) 
 
+def euler_totient(a):
+    total = 0
+    for i in range(a):
+        if extended_euclidean(i, a)[0] == 1:
+            total = total + 1
+    return total
+
+def primitive_root(a,n,psi):
+    roots = set()
+    for i in range(n):
+        roots.add( (a**i)%n )
+    print(roots)
+    #TODO: need to add a thing to get all elements of F_n            
+
+def primitive_roots(a):
+    psi_a = euler_totient(a-1)
+    for i in range( a ):
+        roots = set()
+        for j in range(a):
+            roots.add( (i**j) % a)
+        if len(roots) == psi_a:
+            return roots, len(roots), i
+        print(len(roots))
+    return 'Failed' # Totally different
+ 
+
 if __name__ == '__main__':
     print(ee_multiplicative_inverse(47, 11))
     print(fp_multiplicative_inverse(47, 11))
 
     print(ee_multiplicative_inverse(587, 345))
     print(fp_multiplicative_inverse(587, 345))
+
+    """
+    print('\n1.32')
+    print('phi(229) = ',euler_totient(228))
+    print('Roots: ' , primitive_roots(229))
+    """
+    print('\nTest')
+    print('psi(11) = ', euler_totient(10))
+    primitive_root(2,11)
